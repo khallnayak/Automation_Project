ca60fbbf13c8f5f58f0b3096b2b5ff943b88a474
root <root@ip-172-31-80-174.ec2.internal>
2022-03-05 06:06:38 +0000

    Updating inventroy html file with logs of created backups

--- automation.sh
+++ automation.sh
@@ -15,10 +15,20 @@ if [ $? -ne 0 ]
 then
 	echo "service unable to start"
 else
-	filename=/tmp/mahij-http-logs-$(date '+%d%m%Y-%H%M%S').tar
-	short_file=mahij-http-logs-$(date '+%d%m%Y-%H%M%S').tar
+	timestamp=$(date '+%d%m%Y-%H%M%S')
+	short_file=mahij-http-logs-$timestamp.tar
+	filename=/tmp/$short_file
 	s3_bucket=upgrad-mahij
+	web_file=/var/www/html/inventory.html
 	tar -cf $filename /var/log/apache2/*.log
+	filesize=$(du -h $filename | cut -f 1)
+	if [ -f $web_file ]
+	then
+		echo "httpd-logs    $timestamp    tar	$filesize" >> $web_file
+	else
+		echo "<pre> LogType	DateCreated		Type	Size" > $web_file
+		echo "httpd-logs $timestamp tar $filesize" >> $web_file
+	fi
 	aws s3 cp $filename s3://${s3_bucket}/mahij-http-logs-$(date '+%d%m%Y-%H%M%S').tar
 fi
 
